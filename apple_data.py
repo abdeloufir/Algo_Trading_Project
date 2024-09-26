e539ef829879d4626897dfb67a7a09ef634a8476
abdeloufir <abder.eloufir@gmail.com>
2024-09-27 00:52:26 +0100

    Script to import Apple data used for this project.

--- apple_data.py
+++ apple_data.py
@@ -9,7 +9,7 @@ data_dir = './data'
 os.makedirs(data_dir, exist_ok=True)
 
 # Download historical data for the ticker (daily data for the last 5 years)
-stock_data = yf.download(ticker, period="5y", interval="1d")
+stock_data = yf.download(ticker, period='5y', interval="1d")
 
 # Save the data to a CSV file
 csv_file = os.path.join(data_dir, f'{ticker}_data.csv')
